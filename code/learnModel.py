166697544f1561682db0cc575449b1fe958d3560
Nathan <mission2ridews@gmail.com>
2014-01-16 11:32:44 -0800

    initial commit: directories, outline, gitignore

---
+++ code/learnModel.py
@@ -0,0 +1,7 @@
+"""
+Learn a model of galaxies from the training data
+Usage:
+python src/learnModel.py data/images_training_rev1 models/output.model
+
+The model will be periodically saved with the name models/output.model.temp as well.
+"""
